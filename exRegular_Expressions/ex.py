e409a7ff193fb3fc53a227c60588efdf37992969
Avinav Sharma <41234537+sormazi@users.noreply.github.com>
2019-01-23 18:41:34 +0530

    Add files via upload

--- exRegular_Expressions/ex.py
+++ exRegular_Expressions/ex.py
@@ -1,1 +1,22 @@
+import re
 
+sum = 0
+
+file = open('regex_sum_167634.txt', 'r')
+for line in file:
+    numbers = re.findall('[0-9]+', line)
+    if not numbers:
+        continue
+    else:
+        for number in numbers:
+            sum += int(number)
+
+print(sum)
+
+# += adds another value with the variable's value and assigns the new value to the variable.
+
+x = 3
+x += 2
+print (x)
+5
+# -=, *=, /= does similar for subtraction, multiplication and division.
