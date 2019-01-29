d6ef19480239690d48e478049911930de99efacc
Avinav Sharma <41234537+sormazi@users.noreply.github.com>
2019-01-30 01:43:06 +0530

    Add files via upload

--- ex13_scraping/ex13_scraping.py
+++ ex13_scraping/ex13_scraping.py
@@ -1,1 +1,34 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
 
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the span tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    #print('TAG:', tag)
+    #print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    #print('Attrs:', tag.attrs)
+numbers = [] # make an empty list of numbers
+for span in tags:
+    numbers.append(int(span.string))
+print(len(numbers))
+print(sum(numbers)) # Calculate the sum of the list
